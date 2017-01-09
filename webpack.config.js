88f52c3d958f29e92fb315df891bdaf948b51689
Apondon <yl940830@163.com>
2017-01-09 17:54:48 +0800

    react demo

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+	entry:__dirname + '/src/app.js',
+
+	output:{
+		path: __dirname + '/prd/',
+		filename:'bundle.js'
+	},
+
+	devtool:'source-map',
+
+	// module:{
+	// 	loaders:[
+	// 		{
+	// 			test:/\.css$/,
+	// 			loader:'style-loader!css-loader'
+	// 		},
+  //
+	// 		{
+	// 			test:/\.js$/,
+	// 			loader:'babel-loader?presets[]=es2015'
+	// 		},
+  //
+	// 		{
+	// 			test:/\.scss$/,
+	// 			// loader:'style!css!sass-loader'
+	// 			loader:ET.extract('style-loader','css!sass')
+	// 		},
+  //
+	// 		{
+	// 			test:/\.string$/,
+	// 			loader:'string'
+	// 		},
+  //
+	// 		{
+	// 			test:/\.vue$/,
+	// 			loader:'vue'
+	// 		}
+  //
+	// 	]
+	// },
+}
