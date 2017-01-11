befa5d380f53c68e8cca3b00d93579e794197a0d
Apondon <yl940830@163.com>
2017-01-11 17:14:15 +0800

    mock+images

---
+++ mock/mock.js
@@ -0,0 +1,10 @@
+var banner = require('./banner.json');
+var part = require('./parts.json');
+
+
+module.exports = function() {
+    return {
+        "banner": banner,
+        "parts": part
+    }
+}
