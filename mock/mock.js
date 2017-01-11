1c7d92e96a74b15f548b818c134f80a841e02df5
Apondon <yl940830@163.com>
2017-01-11 17:23:17 +0800

    json+img

--- mock/mock.js
+++ mock/mock.js
@@ -1,10 +1,14 @@
 var banner = require('./banner.json');
 var part = require('./parts.json');
+var video = require('./video.json');
+var more = require('./more.json');
 
 
 module.exports = function() {
     return {
         "banner": banner,
-        "parts": part
+        "parts": part,
+        "video": video,
+        "more": more
     }
 }
