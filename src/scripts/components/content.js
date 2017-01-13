0dab88329fa7e036d0c0bbf69699fff0805378a7
Apondon <yl940830@163.com>
2017-01-13 15:23:28 +0800

    status$

--- src/scripts/components/content.js
+++ src/scripts/components/content.js
@@ -1,14 +1,59 @@
 var Content = React.createClass({
+  getDefaultProps:function(){
+    return{
+      "bannerUrl":"/api/banner",
+      "listUrl":"/api/parts"
+    }
+  },
+  getInitialState:function(){
+    return{
+      "bannerImg" : "",
+      "list" : ""
+    }
+  },
+  componentWillMount:function(){
+    var This = this;
+    fetch(this.props.bannerUrl).then(response => response.json())
+    .then(res => {
+        This.setState({
+					"bannerImg" : res
+				})
+    })
+    .catch(e => console.log("first, error", e));
+
+  },
   render:function(){
+    var arrBanner = [];
+		var bannerImg = this.state.bannerImg;
+		if(bannerImg){
+      var lenB = bannerImg.length;
+			for( var i = 0;i < lenB;i ++ ){
+				arrBanner.push( <div className="swiper-slide"><img src={bannerImg[i].img}/></div>)
+			}
+		}
     return(
       <div className="content">
-        <div className="bannerBox">this is content</div>
+        <div className="bannerBox">
+          <div className="swiper-container">
+            <div className="swiper-wrapper">
+              {arrBanner}
+            </div>
+            <div className="swiper-pagination"></div>
+          </div>
+        </div>
         <div className="indexBox">
           this is list
         </div>
       </div>
 
     )
+  },
+  componentDidMount:function(){
+    console.log(document.getElementsByClassName('swiper-pagination'))
+    swiperOne = new Swiper('.swiper-container', {
+                      pagination: '.swiper-pagination',
+                      autoplay : 2000,
+                   });
   }
 })
 
